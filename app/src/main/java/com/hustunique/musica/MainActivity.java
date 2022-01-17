fb9b5353a8db4bd2be98a909402dbc5019ac5ae2
junkfood <junkfood@hust.edu.cn>
2022-01-18 01:57:34 +0800

    Initial commit

---
+++ app/src/main/java/com/hustunique/musica/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hustunique.musica;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
