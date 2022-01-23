b5cdae746d0e4a5e613f53095fa1706cfe60fc24
cmdMarkcmd <3369305825@qq.com>
2022-01-23 18:40:47 +0800

    update UI of content

--- app/src/main/java/com/hustunique/musica/content/ExactActivity.java
+++ app/src/main/java/com/hustunique/musica/content/ExactActivity.java
@@ -1,6 +1,7 @@
 package com.hustunique.musica.content;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,12 +9,16 @@ import com.hustunique.musica.R;
 
 public class ExactActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
 
         String Id = getIntent().getStringExtra("WID");
+
         // get the information
+        ImageView imageView = (ImageView) findViewById(R.id.PhotoShow);
+        imageView.setBackgroundResource(R.drawable.orange);
     }
 }
