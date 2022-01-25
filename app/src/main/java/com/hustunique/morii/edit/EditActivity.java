3e3a30607ac08c6a33c8af077c95c3f1e4df1439
junkfood <junkfood@hust.edu.cn>
2022-01-26 03:38:58 +0800

    UI Improvement(EditActivity,ContentActivity)

--- app/src/main/java/com/hustunique/morii/edit/EditActivity.java
+++ app/src/main/java/com/hustunique/morii/edit/EditActivity.java
@@ -1,6 +1,7 @@
 package com.hustunique.morii.edit;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
@@ -11,22 +12,27 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.material.textfield.TextInputLayout;
+import com.google.android.material.textfield.TextInputEditText;
+
 import morii.R;
+
 import com.hustunique.morii.content.ContentActivity;
+import com.hustunique.morii.home.MusicDiaryItem;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
-public class EditActivity extends AppCompatActivity implements EditContract.IView{
-    private String _music_name = "",_music_content = "";
-    private TextInputLayout music_name,music_content;
-    private ImageView complete_image,back_image;
-    private TextView complete_text,back_text,textView_hour,textView_week,textView_month;
+public class EditActivity extends AppCompatActivity implements EditContract.IView {
+    private String content = "", title = "";
+    private TextInputEditText textTitle, textContent;
+    private TextView textView_hour, textView_week, textView_month;
+    private ConstraintLayout complete_layout, back_layout;
     private ImageView addPhoto;
     private ImageView showPhoto;
     private String ImagePath = null;
     private EditContract.IPresenter presenter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -39,29 +45,28 @@ public class EditActivity extends AppCompatActivity implements EditContract.IVie
 
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         if (ImagePath != null) showPhoto.setImageBitmap(BitmapFactory.decodeFile(ImagePath));
         super.onResume();
     }
 
     @Override
-    public void setAddPhoto(String path){
+    public void setAddPhoto(String path) {
         ImagePath = path;
         Bitmap bitmap = BitmapFactory.decodeFile(path);
         showPhoto.setImageBitmap(bitmap);
     }
-    private void  initUI(){
-        music_content = findViewById(R.id.textLayoutTitle_music_content);
-        music_name = findViewById(R.id.textLayoutTitle_music_name);
+
+    private void initUI() {
+        textContent = findViewById(R.id.editTextContent);
+        textTitle = findViewById(R.id.editTextTitle);
         textView_hour = findViewById(R.id.textView_hour);
         textView_week = findViewById(R.id.textView_week);
         textView_month = findViewById(R.id.textView_month);
-        complete_image = findViewById(R.id.imageView_edit_complete);
-        back_image = findViewById(R.id.imageView_edit_back);
-        complete_text = findViewById(R.id.textView_edit_complete);
-        back_text = findViewById(R.id.textView_edit_back);
-        addPhoto =  findViewById(R.id.addPhoto);
-        showPhoto =  findViewById(R.id.BigPhoto);
+        complete_layout = findViewById(R.id.completeLayout_edit);
+        back_layout = findViewById(R.id.backLayout_content);
+        addPhoto = findViewById(R.id.addPhoto);
+        showPhoto = findViewById(R.id.BigPhoto);
         textView_hour.setText(getTime("HH:mm"));
         textView_month.setText(getTime("MM月dd日"));
         textView_week.setText(getTime("E"));
@@ -71,72 +76,28 @@ public class EditActivity extends AppCompatActivity implements EditContract.IVie
                 presenter.getPicture();
             }
         });
-        complete_image.setOnClickListener(v->{
-            _music_name = music_name.toString().trim();
-            _music_content = music_content.toString().trim();
-            if(_music_content.length()!=0&&_music_name.length()!=0){
+        complete_layout.setOnClickListener(v -> {
+            content = Objects.requireNonNull(textTitle.getText()).toString();
+            title = Objects.requireNonNull(textContent.getText()).toString();
+            if (title.length() != 0 && content.length() != 0) {
+                MusicDiaryItem musicDiaryItem = new MusicDiaryItem();
+                musicDiaryItem.setTitle(title);
+                musicDiaryItem.setArticle(content);
+                musicDiaryItem.setMusicTabId(getIntent().getIntExtra("musicTabId", 0));
+                musicDiaryItem.setItemID(11);
                 Intent intent = new Intent(this, ContentActivity.class);
+                intent.putExtra("diary", musicDiaryItem);
+                intent.putExtra("NewItem", 1);
                 startActivity(intent);
                 return;
             }
-            Toast.makeText(this,"未输入完全（^.^）",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "未输入完全（^.^）", Toast.LENGTH_SHORT).show();
         });
-        complete_text.setOnClickListener(v->{
-            _music_name = music_name.toString().trim();
-            _music_content = music_content.toString().trim();
-            if(_music_content.length()!=0&&_music_name.length()!=0){
-                Intent intent = new Intent(this, ContentActivity.class);
-                startActivity(intent);
-                return;
-            }
-            Toast.makeText(this,"未输入完全（^.^）",Toast.LENGTH_SHORT).show();
-        });
-        back_text.setOnClickListener(v->{
-            finish();
-        });
-        back_image.setOnClickListener(v->{
-            finish();
-        });
-        /*
-        TextWatcher nameWatcher = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                _music_name = s.toString().trim();
-            }
-        };
-        TextWatcher contentWatcher = new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                _music_content = s.toString().trim();
-            }
-        };
-        music_name.addTextChangedListener(nameWatcher);
-        music_content.addTextChangedListener(contentWatcher);
-
-         */
+        back_layout.setOnClickListener(v -> onBackPressed());
 
     }
-    private String getTime(String format){
+
+    private String getTime(String format) {
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat(format);
         return simpleDateFormat.format(new Date());
     }
